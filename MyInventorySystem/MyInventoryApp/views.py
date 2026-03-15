efe7bbdd8c9313277a7c393473bbcbd901874b69
HifumiKaede <ericjudo2@gmail.com>
2026-03-15 13:42:29 +0800

    base stuff

--- MyInventorySystem/MyInventoryApp/views.py
+++ MyInventorySystem/MyInventoryApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Supplier
 
+def view_supplier(request):
+    suppliers = Supplier.objects.all()
+    return render(request, "view_supplier.html", {"suppliers": suppliers})
 # Create your views here.
+    
