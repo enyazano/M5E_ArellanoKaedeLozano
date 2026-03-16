8528e86440e8a2d5813a380063554b39368c1ca6
enyazano <enyamrgrtlozano@gmail.com>
2026-03-16 21:51:38 +0800

    part c parts for project

--- MyInventorySystem/MyInventoryApp/views.py
+++ MyInventorySystem/MyInventoryApp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from .models import Supplier
+from .models import Supplier, WaterBottle
 
 def view_supplier(request):
     suppliers = Supplier.objects.all()
     return render(request, "view_supplier.html", {"suppliers": suppliers})
-# Create your views here.
+
+# View Bottles View
+def view_bottles(request):
+    bottles = WaterBottle.objects.all()
+    return render(request, "view_bottles.html", {"bottles":bottles})
+
     
